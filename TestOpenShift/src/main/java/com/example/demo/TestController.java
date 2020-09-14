3790680aaa1ebb885e8eb34a8ce09f62dc1eb4cf
Muktar <muktar.prl@gmail.com>
2020-09-14 23:29:55 +0530

    commit changes
    commit code

---
+++ TestOpenShift/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/go")
+	public void test() {
+		System.out.println("Hello World 1");
+		
+		
+	}
+
+}
